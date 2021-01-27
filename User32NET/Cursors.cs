e369c40aea2966d3d700e156e3298e5eaf4074b7
云之梦歌 <taoxinhong@foxmail.com>
2021-01-27 16:14:04 +0800

    初始化

---
+++ User32NET/Cursors.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User32NET
+{
+    /// <summary>
+    /// 鼠标光标操作类
+    /// </summary>
+    public class Cursors
+    {
+        /// <summary>
+        /// 加载鼠标光标文件
+        /// </summary>
+        /// <param name="fileName">.cur文件路径</param>
+        /// <returns>如果成功，则返回光标文件句柄</returns>
+        [DllImport("user32.dll")]
+        public static extern IntPtr LoadCursorFromFile(string fileName);
+
+    }
+}
