dd5a4c792d2b52708cda205f0d598cdf5b2449ac
云之梦歌 <taoxinhong@foxmail.com>
2021-02-21 18:49:42 +0800

    update User32NET/Menus.cs.

--- User32NET/Menus.cs
+++ User32NET/Menus.cs
@@ -8,5 +8,12 @@ namespace User32NET
 	/// </summary>
 	public class Menus
 	{
+        /// <summary>
+		/// 创建一个弹出式菜单。此菜单最初是空的，但可用函数InsertMenuItem，AppendMenu，和InsertMenu来填入菜单项。
+		/// 函数原型：HMENU CreateMenu（VOID）
+		/// </summary>
+		/// <returns>返回新创建菜单的句柄</returns>
+        [DllImport("user32.dll")]
+        public static extern IntPtr CreateMenu();
     }
 }
