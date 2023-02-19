fe728d222958e3136ea1c0828b492b7801f78143
DresnClor <dresnclor@qq.com>
2023-02-19 11:51:11 +0800

    update 02.19-1

---
+++ Gdi32/Drawing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Win32NET.Gdi32
+{
+    /// <summary>
+    /// 绘图操作类
+    /// </summary>
+    public class Drawing
+    {
+
+    }
+}
